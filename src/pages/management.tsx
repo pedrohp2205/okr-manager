2437d3293972f84b548b4beb56f33de9611048b8
pedrohp2205 <hpm1819@gmail.com>
2024-05-06 11:54:46 -0300

    Titulo modifica pelo input do dashboard

--- src/pages/management.tsx
+++ src/pages/management.tsx
@@ -1,4 +1,7 @@
 import { useParams } from "react-router-dom"
+import logo from "../assets/logobanese.png"
+import perfil from "../assets/perfil.png"
+import { ChangeEvent, useState } from 'react';
 
 interface Item {
     id: string;
@@ -12,15 +15,53 @@ export function Management() {
     const { id } = useParams();
     const storedItems = localStorage.getItem("items");
 
-    let specificItem
+
+    let specificItem: Item | undefined;
 
     if (storedItems !== null) {
         const itemsArray: Item[] = JSON.parse(storedItems);
         specificItem = itemsArray.find(item => item.id === id);
     }
     
+    const [editedTitle, editTitle] = useState(specificItem?.title)
 
+    function handleEditTitle(event: ChangeEvent<HTMLInputElement>) {
+        const newTitle = event.target.value;
+        editTitle(newTitle);
+    
+        if (storedItems !== null) {
+            const itemsArray: Item[] = JSON.parse(storedItems);
+            const updatedItemsArray = itemsArray.map(item => {
+                if (item.id === id && specificItem) {
+                    return { ...item, title: newTitle }
+                }
+                return item;
+            });
+    
+            localStorage.setItem("items", JSON.stringify(updatedItemsArray))
+        }
+    }
+    
+
+    
+      
+      
+    
+
+
+    
     return(
-        <div><p>{specificItem?.title}</p></div>
+        <div>
+
+            <header>
+                <nav className=" h-24 px-12 py-3 flex border-b-[1px] border-[#d9d9d9] items-center justify-between">
+                    <img src={logo} alt="Logo Banese" />
+                    <input type="text" className=" text-xl  h-7 w-full max-w-[43.75rem] focus:outline-none border-b border-[#d3d3d3] text-center " onChange={handleEditTitle } value={editedTitle}/>
+
+                    <img src={perfil} alt=""  className="max-w-20 max-h-20" />
+
+                </nav>
+            </header>
+        </div>
     )
 }
