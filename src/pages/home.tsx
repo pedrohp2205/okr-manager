482233851e2bbe24330c15d56283b1ae7e085c35
pedrohp2205 <hpm1819@gmail.com>
2024-05-16 11:16:44 -0300

    Página home mobile friendly

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,27 @@
 import logo from "../assets/logobanese.png"
 import banner from "../assets/banner.png"
 import { Link } from "react-router-dom"
+import bannermobile  from "../assets/bannermobile.png"
+import logomobile from "../assets/logo.png"
 
 export function Home() {
     return(
         <header>
-            <nav className=" h-24 px-12 py-3 flex justify-between">
-                <img src={logo} alt="Logo Banese" />
+            <nav className=" h-24 px-12 py-3 flex justify-between max-md:justify-center">
+                <img src={logo} alt="Logo Banese" className=" max-md:hidden" />
+                <img src={logomobile} alt="" className="hidden max-md:flex max-md:mr-[5rem]"/>
 
-                <div className="flex gap-5">
-                    <Link to= '/login'><button className="w-64 h-16 rounded-[15px] border-green border-2 font-sans text-xl text-green  font-bold p-3 ">Entrar </button></Link>
-                    <Link to= '/register'><button className="w-64 h-16 rounded-[15px] border-green border-2 font-sans text-xl font-bold p-3 bg-green text-white">Cadastre-se</button></Link>
+                <div className="flex gap-5 max-md:items-center max-md:justify-center">
+                    <Link to= '/login'><button className="w-64 h-16 rounded-[15px] border-green border-2 font-sans text-xl text-green  font-bold p-3  max-md:w-[6.25rem] max-md:h-[3.125rem] max-md:text-[0.625rem] max-md:flex max-md:items-center max-md:justify-center">Entrar </button></Link>
+                    <Link to= '/register'><button className="w-64 h-16 rounded-[15px] border-green border-2 font-sans text-xl font-bold p-3 bg-green text-white  max-md:w-[6.25rem] max-md:h-[3.125rem] max-md:text-[0.625rem] max-md:flex max-md:items-center max-md:justify-center">Cadastre-se</button></Link>
 
                 </div>
 
                 
 
             </nav>
-            <img src={banner} alt="" className="w-full"/>
+            <img src={banner} alt="" className="w-full  max-md:hidden"/>
+            <img src={bannermobile} alt="" className="hidden max-md:flex max-md:h-screen max-md:w-full overflow-hidden" />
         </header>
     )
 }
